67d734a37d7c2e9e7d993a0f6b0c9ffbfe29f69c
ilijamitev <94140253+ilijamitev@users.noreply.github.com>
2022-05-28 20:26:39 +0200

    Added Class02

---
+++ Classes/Class02/HW_2_Query1.sql
@@ -0,0 +1,26 @@
+USE SEDC
+GO
+
+--1. Find all Employees with FirstName = Goran
+SELECT * FROM Employees 
+WHERE FirstName = 'Goran';
+
+--2. Find all Employees with LastName starting With ‘S’
+SELECT * FROM Employees
+WHERE LastName LIKE 'S%';
+
+--3. Find all Employees with DateOfBirth greater than ‘01.01.1988’
+SELECT * FROM Employees
+WHERE DateOfBirth > '1988-01-01';
+
+--4. Find all Male employees
+SELECT * FROM Employees
+WHERE Gender = 'M';
+
+--5. Find all employees hired in January/1998
+SELECT * FROM Employees
+WHERE HireDate BETWEEN '1998-01-01' AND '1998-01-31';
+
+--6. Find all Employees with LastName starting With ‘A’ hired in January/1998
+SELECT * FROM Employees
+WHERE HireDate BETWEEN '1998-01-01' AND '1998-01-31' AND LastName LIKE 'A%';
